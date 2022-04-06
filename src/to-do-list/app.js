568c6930ffd81e46e5149c602841cb5a61369f18
Lian Katherin Guevara <liann.guevara@gmail.com>
2022-04-05 21:25:03 -0500

    task completed

--- src/to-do-list/app.js
+++ src/to-do-list/app.js
@@ -4,28 +4,71 @@ const list = document.querySelector('#task__list'); //lista de tareas
 const input = document.querySelector('#input'); // placeholder para agregar la tarea
 const submit = document.querySelector('#submit'); // botón para agregar la tarea
 
-//* 1. función agregar tarea
+const check = 'fa-check-circle';
+const uncheck = 'fa-circle';
+const lineThrough = 'line-through';
+let id= 0;
 
-const addTask = (task) => {
-    const element= `
+//* función agregar tarea
+
+const addTask = (task, id, completed, removed) => {
+
+    if (removed) {
+        return
+    }
+
+    const COMPLETED = completed ? check : uncheck; 
+    const LINE = completed ? lineThrough : ''; 
+
+    const elemento= `
                         <li id="element">
-                            <i class="far fa-circle co" data="finish" id="0"></i>
-                            <h2 class="text">${task}</h2>
-                            <i class="fa-solid fa-trash-can borrar ata="delete" id="0""></i>
+                            <i class="far ${COMPLETED}" data="completed" id="${id}"></i>
+                            <h2 class="text ${LINE}">${task}</h2>
+                            <i class="fa-solid fa-trash-can borrar data="removed" id="${id}"></i>
                         </li> 
                     `
 
-    list.insertAdjacentHTML("beforeend", element);   //element.insertAdjacentHTML(posición, texto);
+    list.insertAdjacentHTML("beforeend", elemento);   //element.insertAdjacentHTML(posición, texto); Agrega cada tarea pero de última
 }
+
+// * Función tarea realizada o eliminada
+    function taskCompleted(element) {
+        element.classList.toggle(check);
+        element.classList.toggle(uncheck);
+        element.parentNode.querySelector('.text').classList.toggle(lineThrough);
+      
+    }
+
+   
+
+
 
 submit.addEventListener('click', (e)  => {
     e.preventDefault (); // Para evitar que se recargue la página
     const task = input.value; //valor del input
-    task ? addTask(task) : input.value = ''; //aplicando condicional ternario
+    // task ? addTask(task) : input.value = ''; //aplicando condicional ternario
     
-    // if (task) {
-    //     addTask(task)
-    // }
-    // input.value = ''
+    if (task) {
+        addTask(task, id, false, false)
+    }
+    input.value = ''
+    id++
 });
 
+//* 2. Finalizar una tarea 
+
+list.addEventListener('click', function(event) {
+    // e.preventDefault (); 
+    const element = event.target
+    const elementData = element.attributes.data.value    
+
+    // console.log(element)
+    // console.log(element.attributes)
+    // console.log(element.attributes.data)
+    // console.log(element.attributes.data.value)
+    if (elementData === 'completed') {
+        taskCompleted(element)
+    }
+    else if (elementData === 'removed')
+        taskRemoved(element)
+});
